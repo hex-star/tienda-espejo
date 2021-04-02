9c71001983b75461c9e88db3150d71b54adfde06
tomas paniego <tomipaniego@gmail.com>
2021-04-02 17:48:24 -0300

    agregue carpeta de fotos

--- tienda/src/App.js
+++ tienda/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Kucci from './Kucci.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={Kucci} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
