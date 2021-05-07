f38e40a8324a42f5cad4e85101e8a684e63a7e2a
Nicolas Estrella <estrella.nico@gmail.com>
2021-05-07 17:36:10 -0300

    agregue mensaje de enviado en Contacto

--- src/Contact.js
+++ src/Contact.js
@@ -14,6 +14,9 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button'
 import { createMuiTheme, withStyles, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import { purple,pink } from '@material-ui/core/colors';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 const BootstrapButton = withStyles({
   root: {
@@ -66,6 +69,19 @@ const ColorButton = withStyles((theme) => ({
 
 function Contact(props) {
   const { cartItemsCount, email } = props;
+  const [open,setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event,reason) => {
+    if(reason === 'clickaway'){
+      return;
+    }
+    setOpen(false);
+  };
+
   return (
     <React.Fragment>
       <AppAppBar cartItemsCount={cartItemsCount} email={email} />
@@ -108,9 +124,29 @@ function Contact(props) {
         </div>
         <br></br>
         <div style={{}}>
-        <ColorButton variant="contained" color="primary">
+        <ColorButton onClick={handleClick} variant="contained" color="primary">
           Enviar consulta
         </ColorButton>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          open={open}
+          autoHideDuration={3000}
+          onClose={handleClose}
+          message='Su consulta fue enviada. Nos contactaremos a la brevedad. ¡Gracias!'
+          action={
+            <React.Fragment>
+            <Button color="secondary" size="small" onClick={handleClose}>
+              
+            </Button>
+            <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </React.Fragment>
+        }
+        />
         </div>
         </div>
       </form>
